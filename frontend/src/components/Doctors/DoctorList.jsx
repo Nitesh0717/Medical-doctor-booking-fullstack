3993a434620fe62dbc57e2ecf328faa345fa92e4
Nitesh0717 <singhnitesh0204@gmail.com>
2026-04-12 00:32:42 +0530

    Refactor DoctorList component for better readability

--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -1,23 +1,34 @@
-
 import DoctorCard from "./DoctorCard";
-import {BASE_URL} from "../../config.js"
-import Error from "../../components/Error/Error.jsx"
-import Loader from "../../components/Loader/Loading.jsx"
-import useFetchData from '../../hooks/useFetchData.js'
+import { BASE_URL } from "../../config.js";
+import Error from "../../components/Error/Error.jsx";
+import Loader from "../../components/Loader/Loading.jsx";
+import useFetchData from "../../hooks/useFetchData.js";
 
 const DoctorList = () => {
-  const {data: doctors, loading, error} = useFetchData(`${BASE_URL}/doctors`)
+
+  const {
+    data: doctors = [],
+    loading,
+    error,
+  } = useFetchData(`${BASE_URL}/doctors`);
+
   return (
     <div>
-        {loading && <Loader/>}
-        {error && <Error errMsg={error}/>}
-        {!error && !loading && (
-            <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-8 mt-[30px]">
-              {doctors.map((doctor,index ) => (
-                 index < 3 && (<DoctorCard doctor={doctor} key={doctor._id} />)
-              ))}
-          </div>
-        )}
+
+      {loading && <Loader />}
+
+      {error && <Error errMsg={error} />}
+
+      {!loading && !error && (
+        <div className="grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2 gap-8 mt-[30px]">
+
+          {doctors?.slice(0, 3)?.map((doctor) => (
+            <DoctorCard doctor={doctor} key={doctor?._id} />
+          ))}
+
+        </div>
+      )}
+
     </div>
   );
 };
