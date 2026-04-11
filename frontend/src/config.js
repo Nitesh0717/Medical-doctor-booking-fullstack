6606a0e860ffb8905729b124814eed55081f899b
Nitesh0717 <singhnitesh0204@gmail.com>
2026-04-11 17:31:47 +0530

    Fix token retrieval syntax in config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const url_production = "https://doctor-backend-0wvd.onrender.com"
 
-// eslint-disable-next-line no-unused-vars
 const url_local = "http://localhost:5000"
 
 export const BASE_URL = url_production;
-export const token = localStorage.getItem('token');
+
+export const token = localStorage.getItem("token");
