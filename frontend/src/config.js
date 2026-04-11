654a17c9012eea8ebc7f0b8cc3822ad5ab614e97
Nitesh0717 <singhnitesh0204@gmail.com>
2026-04-11 17:22:50 +0530

    correct backend production url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,7 @@
+const url_production = "https://doctor-backend-0wvd.onrender.com"
 
-const url_production = "https://medicare-backend-1-3tho.onrender.com"
 // eslint-disable-next-line no-unused-vars
 const url_local = "http://localhost:5000"
+
 export const BASE_URL = url_production;
 export const token = localStorage.getItem('token');
